93bf9ce981e23b99aa72de69771a1dfcebfc1b85
saif-abualkhair <saif_abualkhair@hotmail.com>
2022-06-16 19:43:03 +0300

    refactoring content area into a component for reusability

---
+++ src/app/shared/content-area/content-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lta-content-area',
+  templateUrl: './content-area.component.html',
+  styleUrls: ['./content-area.component.css']
+})
+export class ContentAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
