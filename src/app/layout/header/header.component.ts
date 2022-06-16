c384c863f2daa60c345d9dfef04a2e7f1035c6f9
saif-abualkhair <saif_abualkhair@hotmail.com>
2022-06-16 19:45:17 +0300

    refactor and design

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'lta-header',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isSearchVisable: boolean = false;
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
+    this.location.onUrlChange(v => {
+      this.isSearchVisable = v !== '/' ? true : false;
+    })
   }
-
 }
