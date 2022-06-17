f0e4d10e3895898d9cda8b0603591be60d88b53f
saif-abualkhair <saif_abualkhair@hotmail.com>
2022-06-17 14:55:48 +0300

    fixed a bug in the blog_posts and made the component more reusable by adding parameters to it

--- src/app/shared/blog-posts/blog-posts.component.ts
+++ src/app/shared/blog-posts/blog-posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lta-blog-posts',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPostsComponent implements OnInit {
 
+  @Input() title?: string;
+  @Input() posts?: any[];
   constructor() { }
 
   ngOnInit(): void {
