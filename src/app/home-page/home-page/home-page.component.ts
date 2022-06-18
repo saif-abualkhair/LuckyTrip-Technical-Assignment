31e3a62df2899a3abe1424fbb3aeddf1defbb945
saif-abualkhair <saif_abualkhair@hotmail.com>
2022-06-18 15:56:30 +0300

    added routing functionalities on submit

--- src/app/home-page/home-page/home-page.component.ts
+++ src/app/home-page/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lta-home-page',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  formGroup!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.buildSearchForm();
   }
 
+  buildSearchForm() {
+    this.formGroup = this.formBuilder.group({
+      mainSearch: [null]
+    })
+  }
+
+  routeToDestination() {
+    this.router.navigate(['/Destination', { id: this.formGroup.value.mainSearch.id }])
+  }
 }
